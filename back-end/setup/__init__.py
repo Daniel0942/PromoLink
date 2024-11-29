81837d0249b9b8e75641bfe66002ea47f1c91c72
DanielBomfim22 <danielufc131@gmail.com>
2024-11-29 16:17:25 -0300

    qwrqef

--- back-end/setup/__init__.py
+++ back-end/setup/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"*": {"origins": "*"},
-"methods": ["GET", "POST", "DELETE", "OPTIONS"]})
+CORS(app, resources={r"/*": {
+    "origins": ["http://localhost:3000", "https://promo-link.vercel.app/"],
+    "methods": ["GET", "POST", "DELETE", "OPTIONS"],
+    "allow_headers": ["Content-Type", "Authorization"]
+}})
 
 from setup.controllers import default, produtos, historico, favoritos
