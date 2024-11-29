f569a224a465d684c54517390dd472b281a99fe9
DanielBomfim22 <danielufc131@gmail.com>
2024-11-29 16:11:30 -0300

    melhorando cada vez mais o cors

--- back-end/setup/__init__.py
+++ back-end/setup/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"*": {"origins": "*"},
+"methods": ["GET", "POST", "DELETE"]})
 
 from setup.controllers import default, produtos, historico, favoritos
