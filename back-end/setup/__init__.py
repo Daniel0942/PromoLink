3f029b33901a7d5cd2e34bbe8e3c4a612c2ef27c
DanielBomfim22 <danielufc131@gmail.com>
2024-11-29 16:24:21 -0300

    faf

--- back-end/setup/__init__.py
+++ back-end/setup/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {
-    "origins": ["http://localhost:3000", "https://promo-link.vercel.app/"],
+    "origins": "*",
     "methods": ["GET", "POST", "DELETE", "OPTIONS"],
     "allow_headers": ["Content-Type", "Authorization"]
 }})
