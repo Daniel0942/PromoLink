db854af2a04c01ff677060992f4701dfde7ad0d6
Daniel <danielufc131@gmail.com>
2024-12-06 21:16:59 -0300

    ajustando

--- back-end/setup/__init__.py
+++ back-end/setup/__init__.py
@@ -4,4 +4,4 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-from setup.controllers import default, produtos, historico, favoritos, pesquisa
+from setup.controllers import login, produtos, historico, favoritos, pesquisa
