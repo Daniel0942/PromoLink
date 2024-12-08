858ebfdb9115dc0667d2d9b619e90dab68c5a928
Daniel <danielufc131@gmail.com>
2024-12-08 20:39:21 -0300

    ajustando produtos back e front

--- back-end/setup/controllers/produtos.py
+++ back-end/setup/controllers/produtos.py
@@ -3,35 +3,57 @@ from flask import jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from setup.BuscadorProdutos.buscador import ProdutosGet
 
-dados_produtos = []
+produtos_kabum = []
+produtos_casasBahia = []
+produtos_magazine = []
 
-@app.route("/produtos")
+@app.route("/produtos/kabum")
 @jwt_required()
-def produtos_index():
-    if len(dados_produtos) > 0:
-        return jsonify(dados_produtos), 200
+def kabum():
+    if len(produtos_kabum) > 0:
+        return jsonify(produtos_kabum), 200
+    else:
+        return jsonify({"Error": "Sem produtos!"}), 500
+
+@app.route("/produtos/casasBahia")
+@jwt_required()
+def casasBahia():
+    if len(produtos_casasBahia) > 0:
+        return jsonify(produtos_casasBahia), 200
+    else:
+        return jsonify({"Error": "Sem produtos!"}), 500
+
+@app.route("/produtos/magazine")
+@jwt_required()
+def magazine():
+    if len(produtos_magazine) > 0:
+        return jsonify(produtos_magazine), 200
     else:
         return jsonify({"Error": "Sem produtos!"}), 500
 
 @app.route("/produtos/<string:site>", methods=["POST"])
 @jwt_required()
 def receber_requisisao(site):
-    # Referencia a variável global para armazenar os resultados
-    global dados_produtos  
+    global produtos_casasBahia, produtos_kabum, produtos_magazine
 
     # Pegar o usuário atual do token recebido
     current_user = get_jwt_identity()
 
-    if dados_produtos:
-        dados_produtos.clear()
-
     instancia = ProdutosGet()
     if "Magazine" in site:
-        dados_produtos = instancia.magazine()
+        if produtos_magazine:
+            produtos_magazine.clear()
+        produtos_magazine = instancia.magazine()
+        print(produtos_magazine)
     elif "Casas" in site:
-        dados_produtos = instancia.casasBahia()
+        if produtos_casasBahia:
+            produtos_casasBahia.clear()
+        produtos_casasBahia = instancia.casasBahia()
+        print(produtos_casasBahia)
     elif "Kabum" in site:
-        dados_produtos = instancia.kabum()
+        if produtos_kabum:
+            produtos_kabum.clear()
+        produtos_kabum = instancia.kabum()
     else:
         return jsonify({"Error": "Site não suportado!"}), 400
 
